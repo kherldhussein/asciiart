d719c59c31b570781f623dfe0e68636ff7c6cce1
khahussein <kherld.hussein@gmail.com>
2024-04-26 20:44:00 +0300

    implement func call on main

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+	asciMap, err := ReadASCIIMapFromFile("standard.txt")
+	if err != nil {
+		fmt.Println("Error reading ASCII map:", err)
+		return
+	}
+	if len(args) < 1 {
+		fmt.Println("Please provide an input string.")
+		return
+	}
+	if len(args) > 1 {
+		fmt.Println("Too many arguments passed")
+		return
+	}
+	err = PrintArt(args[0], asciMap)
+	if err != nil {
+		log.Fatal("unsupported escape sequence")
+	}
+}
+
+// TODO
+// 2 \n
+
+// Test
