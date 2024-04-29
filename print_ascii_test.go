c0d68001bdf100a76bf89ad0c627578f6ddfa3a3
khahussein <kherld.hussein@gmail.com>
2024-04-29 22:42:12 +0300

    impl print ascii test

--- print_ascii_test.go
+++ print_ascii_test.go
@@ -1,41 +1,42 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestPrintArt(t *testing.T) {
-	tests := []struct {
-		name      string
-		input     string
-		asciMap   [][]string
-		expectErr bool
+	asciMap, err := ReadASCIIMapFromFile("standard.txt")
+	if err != nil {
+		t.Errorf("Error reading ASCII map: %v", err)
+		return
+	}
+
+	testCases := []struct {
+		name        string
+		input       string
+		expected    string
+		expectedErr bool
 	}{
 		{
-			name:      "Empty string",
-			input:     "",
-			asciMap:   nil,
-			expectErr: false,
+			name:        "Print a single word",
+			input:       "Hello",
+			expected:    " _    _          _   _               \n| |  | |        | | | |              \n| |__| |   ___  | | | |   ___        \n|  __  |  / _ \\ | | | |  / _ \\       \n| |  | | |  __/ | | | | | (_) |      \n|_|  |_|  \\___| |_| |_|  \\___/       \n                                    \n                                    \n",
+			expectedErr: false,
 		},
 		{
-			name:      "Newline escape sequence",
-			input:     "\\n",
-			asciMap:   nil,
-			expectErr: false,
-		},
-		{
-			name:      "Unsupported escape sequence",
-			input:     "\\t",
-			asciMap:   nil,
-			expectErr: true,
+			name:        "Print unsupported escape sequence",
+			input:       "\\t",
+			expected:    "",
+			expectedErr: true,
 		},
 	}
-	for _, tt := range tests {
+
+	for _, tt := range testCases {
 		t.Run(tt.name, func(t *testing.T) {
-			err := PrintArt(tt.input, tt.asciMap)
-			if tt.expectErr && err == nil {
-				t.Errorf("Expected an error but got none")
-			}
-			if !tt.expectErr && err != nil {
-				t.Errorf("Unexpected error: %v", err)
+			err := PrintArt(tt.input, asciMap)
+			if (err != nil) != tt.expectedErr {
+				t.Errorf("Go (%q) error = %v, Expected %v", tt.input, err, tt.expectedErr)
+				return
 			}
 		})
 	}
