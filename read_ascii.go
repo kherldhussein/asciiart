d14523fa3648ed0d2430a5dca8bcb573c0d8143f
khahussein <kherld.hussein@gmail.com>
2024-04-30 00:21:05 +0300

    handle EOF error in read ascii

--- read_ascii.go
+++ read_ascii.go
@@ -16,6 +16,13 @@ func ReadASCIIMapFromFile(filename string) ([][]string, error) {
 		asciMap  [][]string
 		asciLine []string
 	)
+	// files, err := file.Stat()
+	// if err != nil {
+	// 	return nil, fmt.Errorf("trror getting file stat %v", err)
+	// }
+	// if files.Size() != 6623 {
+	// 	return nil, fmt.Errorf("the files are formatted in a way that is not necessary to change them")
+	// }
 	scanner := bufio.NewScanner(file)
 	count := 0
 	for scanner.Scan() {
@@ -27,6 +34,9 @@ func ReadASCIIMapFromFile(filename string) ([][]string, error) {
 			count = 0
 			asciLine = []string{}
 		}
+	}
+	if len(asciMap) != 95 {
+		return nil, fmt.Errorf("the files are formatted in a way that is not necessary to change them")
 	}
 	if err := scanner.Err(); err != nil {
 		return nil, fmt.Errorf("error scanning file: %w", err)
